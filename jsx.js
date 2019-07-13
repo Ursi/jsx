e21aae427cadaddc6e3159c41d095b04f620a85c
Mason Mackaman <masondeanm@aol.com>
2019-07-13 16:14:19 -0400

    Add setx.js

--- jsx.js
+++ jsx.js
@@ -1,5 +1,6 @@
 import arrayx from './arrayx.js'; export {arrayx};
 import mathx from './mathx.js'; export {mathx};
+import setx from './setx.js'; export {setx};
 import stringx from './stringx.js'; export {stringx};
 import buildIn from './buildIn.js';
 
@@ -28,6 +29,7 @@ Object.defineProperties(jsx, {
 	    value: {
 			arrayx: arrayx,
 			mathx: mathx,
+			setx: setx,
 			stringx: stringx,
 	    },
 	},
---
+++ setx.js
@@ -0,0 +1,17 @@
+import buildIn from './buildIn.js';
+
+const setx = {
+    toArray() {
+        let array = [];
+		for (let entry of this.values()) {
+		    array.push(entry);
+		}
+
+		return array;
+    },
+};
+
+Object.defineProperty(setx, 'buildIn', {value: buildIn(Set.prototype)});
+
+export default setx;
+export {setx};
