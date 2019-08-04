fcae880e3923b8489fb4db400f83208ac97f23a0
Mason Mackaman <masondeanm@aol.com>
2019-08-04 00:44:49 -0400

    Move ii to String.ii

--- jsx.js
+++ jsx.js
@@ -4,32 +4,7 @@ import setx from './setx.js'; export {setx};
 import stringx from './stringx.js'; export {stringx};
 import buildIn from './buildIn.js';
 
-const jsx = {
-	ii(literals, ...params) {
-		let str = '';
-		for (var i = 0; i < params.length; i++) {
-			let literal = literals[i].match(/(?<rest>.*?)(?<tabs>\t*)$/s).groups;
-			str += literal.rest;
-			str += params[i].split('\n').map(line => literal.tabs + line).join('\n');
-		}
-
-		str += literals[i];
-		function leadingTabs(str) {
-			return str.match(/^\t*/)[0].length;
-		}
-
-		let
-			lines = str.split('\n'),
-			addedIndent = Number(lines[0][0]) || 0,
-			baseIndent = leadingTabs(lines[1]);
-
-		return lines.slice(1, -1)
-			.map(line => {
-				let tabNum = Math.max(leadingTabs(line) - baseIndent + addedIndent, 0);
-				return '\t'.repeat(tabNum) + line.replace(/^\t*/, '');
-			}).join('\n');
-	}
-};
+const jsx = {};
 
 Object.defineProperties(jsx, {
 	buildIn: {
--- stringx.js
+++ stringx.js
@@ -2,9 +2,30 @@ import buildIn from './buildIn.js';
 
 const stringx = {
 	String: {
-		test() {
-			console.log("this is from String");
-		},
+		ii(literals, ...params) {
+			let str = '';
+			for (var i = 0; i < params.length; i++) {
+				let literal = literals[i].match(/(?<rest>.*?)(?<tabs>\t*)$/s).groups;
+				str += literal.rest;
+				str += params[i].split('\n').map(line => literal.tabs + line).join('\n');
+			}
+
+			str += literals[i];
+			function leadingTabs(str) {
+				return str.match(/^\t*/)[0].length;
+			}
+
+			let
+				lines = str.split('\n'),
+				addedIndent = Number(lines[0][0]) || 0,
+				baseIndent = leadingTabs(lines[1]);
+
+			return lines.slice(1, -1)
+				.map(line => {
+					let tabNum = Math.max(leadingTabs(line) - baseIndent + addedIndent, 0);
+					return '\t'.repeat(tabNum) + line.replace(/^\t*/, '');
+				}).join('\n');
+		}
 	},
 	toReStr(re = false){
 		let str = this.replace(/\\/g, '\\\\').replace(/([$\(\)*+\.?\[\]^])/g, '\\$1');
