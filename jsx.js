9e0307a7f3b6a838990934ed8e65c6382c3454a2
Mason Mackaman <masondeanm@aol.com>
2019-08-03 21:39:27 -0400

    Add ii
    ii ("ignore indentaion"/"indent(ation) ignore") is a tag function for
    template literals that let's you use them at any level of indentation

--- jsx.js
+++ jsx.js
@@ -4,7 +4,31 @@ import setx from './setx.js'; export {setx};
 import stringx from './stringx.js'; export {stringx};
 import buildIn from './buildIn.js';
 
-const jsx = {};
+const jsx = {
+	ii(literals, ...params) {
+		let str = '';
+		for (var i = 0; i < params.length; i++) {
+			str += literals[i];
+			str += params[i];
+		}
+
+		str += literals[i];
+		function leadingTabs(str) {
+			return str.match(/^\t*/)[0].length;
+		}
+
+		let
+			lines = str.split('\n'),
+			addedIndent = Number(lines[0][0]) || 0,
+			baseIndent = leadingTabs(lines[1]);
+
+		return lines.slice(1, -1)
+			.map(line => {
+				let tabNum = Math.max(leadingTabs(line) - baseIndent + addedIndent, 0);
+				return '\t'.repeat(tabNum) + line.replace(/^\t*/, '');
+			}).join('\n');
+	}
+};
 
 Object.defineProperties(jsx, {
 	buildIn: {
