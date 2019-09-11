e61403848b030a0355c4424f4a73248c99ce8146
Mason Mackaman <masondeanm@aol.com>
2019-09-11 18:47:10 -0400

    Add selfGenerator

--- jsx.js
+++ jsx.js
@@ -4,7 +4,33 @@ import setx from './setx.js'; export {setx};
 import stringx from './stringx.js'; export {stringx};
 import buildIn from './buildIn.js';
 
-const jsx = {};
+const jsx = {
+	SelfGenerator(gf) {
+		let genHolder = new Function;
+		function sg(...params) {
+			let gen = gf.apply(this, params);
+			genHolder.gen = gen;
+			return gen;
+		}
+
+		Object.setPrototypeOf(sg, new Proxy(genHolder, {
+			get(target, prop) {
+				const {gen} = target;
+				if (gen && prop in gen) {
+					if (gen[prop] instanceof Function) {
+						return gen[prop].bind(gen);
+					} else {
+						return gen[prop];
+					}
+				} else {
+					return genHolder[prop]; //sg needs genHolder's properties because it no longer has Function.prototype as its prototype
+				}
+			},
+		}));
+
+		return sg;
+	}
+};
 
 Object.defineProperties(jsx, {
 	buildIn: {
